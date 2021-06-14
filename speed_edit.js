30023deff0c8333b90f1de667ff488524ec0265b
PECO <peco@PECOnoMacBook-Pro-2.local>
2021-06-14 18:37:06 +0900

    the first commit

---
+++ speed_edit.js
@@ -0,0 +1,5 @@
+var videos = document.querySelectorAll("video");
+for (var video of videos){
+    video.playbackRate = 1.8; //the value is random.
+    // you can change the above value as you like
+}
